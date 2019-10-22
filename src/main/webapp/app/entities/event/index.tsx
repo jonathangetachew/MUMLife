c1c0edf9f6ad3822f83c8d9affb11ed79bd3269f
Yadir Hernandez <yadirhb@gmail.com>
2019-10-22 14:40:45 -0500

    [CA-13] Added event list in student landing page.

--- src/main/webapp/app/entities/event/index.tsx
+++ src/main/webapp/app/entities/event/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { AUTHORITIES } from 'app/config/constants';
+import { hasAnyAuthority } from 'app/shared/auth/private-route';
 
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
@@ -8,16 +12,22 @@ import EventDetail from './event-detail';
 import EventUpdate from './event-update';
 import EventDeleteDialog from './event-delete-dialog';
 
-const Routes = ({ match }) => (
-  <>
-    <Switch>
-      <ErrorBoundaryRoute exact path={`${match.url}/new`} component={EventUpdate} />
-      <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={EventUpdate} />
-      <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={EventDetail} />
-      <ErrorBoundaryRoute path={match.url} component={Event} />
-    </Switch>
-    <ErrorBoundaryRoute path={`${match.url}/:id/delete`} component={EventDeleteDialog} />
-  </>
+const Routes = ({ match, isEditable }) => (
+ <>
+   <Switch>
+     { isEditable && <ErrorBoundaryRoute exact path={`${match.url}/new`} component={EventUpdate} />}
+     { isEditable && <ErrorBoundaryRoute exact path={`${match.url}/:id/edit`} component={EventUpdate} />}
+     <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={EventDetail} />
+     <ErrorBoundaryRoute path={match.url} component={Event} />
+   </Switch>
+   { isEditable && <ErrorBoundaryRoute path={`${match.url}/:id/delete`} component={EventDeleteDialog} />}
+ </>
 );
 
-export default Routes;
+const mapStateToProps = ({authentication}) => ({
+  account: authentication.account,
+  isEditable: hasAnyAuthority(authentication.account.authorities, [AUTHORITIES.ADMIN, AUTHORITIES.ORGANIZER]),
+  isAuthenticated: authentication.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Routes);
